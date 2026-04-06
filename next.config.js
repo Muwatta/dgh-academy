9fe1257096c73809124c0a0b7e6dc1c96ae9a226
Muwatta <abdullahmusliudeen@gmail.com>
2026-04-06 10:08:40 +0100

    Replace next.config.ts with next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
